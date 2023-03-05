ba0ce09b1ecca9e6b0e80f8193608d45f625d3bc
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-03-05 19:30:03 +0530

    main-text change

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -61,8 +61,8 @@ function Hero()
     return(
         <div className="container-fluid">
         <div className="row justify-content-evenly align-items-center hero-row flex-wrap-reverse">
-            <div className="col-md-5 px-4 py-2">
-                <h1>Start your Dream Career with CodingWiz</h1>
+            <div className="col-md-6 px-4 py-2">
+                <h1 className='start-text'>Start your Dream Career with <span className='codingwiztext'>CodingWiz</span></h1>
                 <p>Be job ready by learning top & trending technologies used by leading MNCs at just starting price from ₹499!</p>
                 <button className="hero-btn" onClick={CoursesHandler}>Explore Courses</button>
                 <a href="tel:9623235838" className="callTutors"><button className="hero-btn2">Call Tutors</button></a>
