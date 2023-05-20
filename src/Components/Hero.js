2b5436a5daa19c7528a9b940051384e7b585de96
Siddesh Jaiswal <siddeshjaiswal12@gmail.com>
2023-05-20 20:45:22 +0530

    vkll

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -55,7 +55,7 @@ function Hero()
     useEffect(() => {
         const timer = setTimeout(()=>{
             setshowDialog(true)
-        }, 10000)
+        }, 600000)
         return ()=>clearTimeout(timer)
     }, [])
     return(
